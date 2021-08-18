18c928ba2531df8088e9c949c5c3bb95fecda0b1
Shing Ho Tan <talenteyedev@gmail.com>
2021-08-17 21:57:28 -0400

    fix: update fade scroll up for about page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,6 +6,7 @@ import Link from 'next/link';
 import { ArrowRightIcon } from "@heroicons/react/outline";
 import AOS from 'aos';
 import 'aos/dist/aos.css';
+import Fade from 'react-reveal/Fade';
 
 import Header from '../components/Header'
 import Footer from '../components/Footer'
@@ -99,100 +100,106 @@ const Home: NextPage = () => {
           <Image src={AboutImage2} alt='About Image2' layout='intrinsic' />
         </div>
       </div>
-      <div className='flex flex-col xl:mx-auto lg:mx-16 md:mx-10 about-ora-description sm:px-20 sm:py-16 sm:mt-28' data-aos='about-animation'>
-        <div className='about-ora-description-title about-text-blue'>
-          Ora is on a mission to re-invent and modernize the data industry
-        </div>
-        <div className='about-ora-description-subtitle about-text-blue sm:mt-12'>
-          Ora offers online users: 
-        </div>
-        <div className='about-ora-description-subtitle sm:mt-3 flex items-center'>
-          <TickSvg width={23.62} height={16.66} fill={"#2E3192"} />
-          <div className='about-ora-description-content sm:ml-3'>
-            The <span className='font-bold'>most lucrative rewards</span> for their data.
+      <Fade bottom>
+        <div className='flex flex-col xl:mx-auto lg:mx-16 md:mx-10 about-ora-description sm:px-20 sm:py-16 sm:mt-28'>
+          <div className='about-ora-description-title about-text-blue'>
+            Ora is on a mission to re-invent and modernize the data industry
+          </div>
+          <div className='about-ora-description-subtitle about-text-blue sm:mt-12'>
+            Ora offers online users: 
+          </div>
+          <div className='about-ora-description-subtitle sm:mt-3 flex items-center'>
+            <TickSvg width={23.62} height={16.66} fill={"#2E3192"} />
+            <div className='about-ora-description-content sm:ml-3'>
+              The <span className='font-bold'>most lucrative rewards</span> for their data.
+            </div>
+          </div>
+          <div className='about-ora-description-subtitle flex items-center'>
+            <TickSvg width={23.62} height={16.66} fill={"#2E3192"} />
+            <div className='about-ora-description-content sm:ml-3'>
+              Complete privacy with <span className='font-bold'>no name or exact location linked to data.</span>
+            </div>
+          </div>
+          <div className='about-ora-description-subtitle about-text-blue sm:mt-8'>
+            Ora offers online consumers: 
+          </div>
+          <div className='about-ora-description-subtitle sm:mt-3 flex items-center'>
+            <TickSvg width={23.62} height={16.66} fill={"#2E3192"} />
+            <div className='about-ora-description-content sm:ml-3'>
+              The <span className='font-bold'>most detailed, accurate real-time data avaliable anywhere.</span>
+            </div>
+          </div>
+          <div className='about-ora-description-subtitle flex items-center'>
+            <TickSvg width={23.62} height={16.66} fill={"#2E3192"} />
+            <div className='about-ora-description-content sm:ml-3'>
+              The ability to <span className='font-bold'>initiate data requests</span> and gain unprecedented <span className='font-bold'>real-time market insights.</span>
+            </div>
+          </div>
+          <div className='about-ora-description-footer about-text-blue sm:mt-12'>
+            It is no secret that the most valuable commodity for humans is time! It is no coincidence that
+            Ora’s ecosystem is powered by a utility token called TIME.
           </div>
         </div>
-        <div className='about-ora-description-subtitle flex items-center'>
-          <TickSvg width={23.62} height={16.66} fill={"#2E3192"} />
-          <div className='about-ora-description-content sm:ml-3'>
-            Complete privacy with <span className='font-bold'>no name or exact location linked to data.</span>
+      </Fade>
+      <Fade bottom>
+        <div className='flex flex-col 2xl:mx-auto xl:mx-24 lg:mx-12 md:mx-8 items-center about-ora-ecosystem-back sm:px-20 sm:py-16 sm:mt-28'>
+          <div className='about-ora-ecosystem-title about-text-blue'>
+            TIME powers the Ora ecosystem.
+          </div>
+          <div className='sm:mt-20 sm:mb-32'>
+            <Image src={EcoSystemImage} alt='EcoSystem Image' />
           </div>
         </div>
-        <div className='about-ora-description-subtitle about-text-blue sm:mt-8'>
-          Ora offers online consumers: 
-        </div>
-        <div className='about-ora-description-subtitle sm:mt-3 flex items-center'>
-          <TickSvg width={23.62} height={16.66} fill={"#2E3192"} />
-          <div className='about-ora-description-content sm:ml-3'>
-            The <span className='font-bold'>most detailed, accurate real-time data avaliable anywhere.</span>
+      </Fade>
+      <Fade bottom>
+        <div className='about-how-it-works flex flex-col xl:mx-auto lg:mx-16 md:mx-8' data-aos='about-animation'>
+          <div className='about-how-it-works-title flex justify-center'>
+            How It Works
           </div>
-        </div>
-        <div className='about-ora-description-subtitle flex items-center'>
-          <TickSvg width={23.62} height={16.66} fill={"#2E3192"} />
-          <div className='about-ora-description-content sm:ml-3'>
-            The ability to <span className='font-bold'>initiate data requests</span> and gain unprecedented <span className='font-bold'>real-time market insights.</span>
+          <div className='about-ora-description-title about-text-blue sm:mt-10'>
+            Registration
           </div>
+          <ul className='list-disc about-ora-description-content list-outside sm:mt-2 sm:ml-6'>
+            <li>
+              Download and install the FREE Ora app on your mobile phone (Android and iOS available).
+            </li>
+            <li>
+              Upon registration, your account will receive a generous TIME reward.
+            </li>
+            <li>
+              Turn on Ora app notifications to receive bonus TIME rewards.
+            </li>
+            <li>
+              Use a friends referral code (even if already signed up) and send out yours to receive bonus TIME rewards.
+            </li>
+            <li>
+              You are now all set to receive data requests and continued rewards!
+            </li>
+          </ul>
+          <div className='border-b sm:my-6'>
+          </div>
+          <div className='about-ora-description-title about-text-blue'>
+            After Registration
+          </div>
+          <ul className='list-disc about-ora-description-content list-outside sm:mt-2 sm:ml-6'>
+            <li>
+              Eligible users will receive a notification that there is a new data request available.
+            </li>
+            <li>
+              Click a few multiple choice answers in less than 30 seconds and receive TIME for every answer provided.
+            </li>
+            <li>
+              Grow your TIME by staking on chain to receive generous staking rewards (see whitepaper for details).
+            </li>
+            <li>
+              Collected metadata is made available to data consumers.  The privacy of the user’s name and the exact location is protected.
+            </li>
+            <li>
+              Refer as many people as you like to earn ongoing TIME rewards from friends and family’s data!
+            </li>
+          </ul>
         </div>
-        <div className='about-ora-description-footer about-text-blue sm:mt-12'>
-          It is no secret that the most valuable commodity for humans is time! It is no coincidence that
-          Ora’s ecosystem is powered by a utility token called TIME.
-        </div>
-      </div>
-      <div className='flex flex-col 2xl:mx-auto xl:mx-24 lg:mx-12 md:mx-8 items-center about-ora-ecosystem-back sm:px-20 sm:py-16 sm:mt-28' data-aos='about-animation'>
-        <div className='about-ora-ecosystem-title about-text-blue'>
-          TIME powers the Ora ecosystem.
-        </div>
-        <div className='sm:mt-20 sm:mb-32'>
-          <Image src={EcoSystemImage} alt='EcoSystem Image' />
-        </div>
-      </div>
-      <div className='about-how-it-works flex flex-col xl:mx-auto lg:mx-16 md:mx-8' data-aos='about-animation'>
-        <div className='about-how-it-works-title flex justify-center'>
-          How It Works
-        </div>
-        <div className='about-ora-description-title about-text-blue sm:mt-10'>
-          Registration
-        </div>
-        <ul className='list-disc about-ora-description-content list-outside sm:mt-2 sm:ml-6'>
-          <li>
-            Download and install the FREE Ora app on your mobile phone (Android and iOS available).
-          </li>
-          <li>
-            Upon registration, your account will receive a generous TIME reward.
-          </li>
-          <li>
-            Turn on Ora app notifications to receive bonus TIME rewards.
-          </li>
-          <li>
-            Use a friends referral code (even if already signed up) and send out yours to receive bonus TIME rewards.
-          </li>
-          <li>
-            You are now all set to receive data requests and continued rewards!
-          </li>
-        </ul>
-        <div className='border-b sm:my-6'>
-        </div>
-        <div className='about-ora-description-title about-text-blue'>
-          After Registration
-        </div>
-        <ul className='list-disc about-ora-description-content list-outside sm:mt-2 sm:ml-6'>
-          <li>
-            Eligible users will receive a notification that there is a new data request available.
-          </li>
-          <li>
-            Click a few multiple choice answers in less than 30 seconds and receive TIME for every answer provided.
-          </li>
-          <li>
-            Grow your TIME by staking on chain to receive generous staking rewards (see whitepaper for details).
-          </li>
-          <li>
-            Collected metadata is made available to data consumers.  The privacy of the user’s name and the exact location is protected.
-          </li>
-          <li>
-            Refer as many people as you like to earn ongoing TIME rewards from friends and family’s data!
-          </li>
-        </ul>
-      </div>
+      </Fade>
       <div className='sm:mx-auto flex about-learn-more sm:mt-12'>
         <div className='about-text-blue'>
           Learn more by reading our
