11e2d2ce18606fb21266778f0fdef0c181425f7d
Shing Ho Tan <talenteyedev@gmail.com>
2021-08-19 14:48:20 -0400

    fix: ssl

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -55,7 +55,13 @@ const Home: NextPage = () => {
   }, []);
 
   async function onRegister() {
-    process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
+    const fetch = require('node-fetch');
+    const https = require('https');
+
+    const httpsAgent = new https.Agent({
+      rejectUnauthorized: false,
+    });
+
     const res = await fetch(
       'https://104.154.199.36:3001/add',
       {
@@ -65,17 +71,20 @@ const Home: NextPage = () => {
         headers: {
           'Content-Type': 'application/json'
         },
-        method: 'POST'
+        method: 'POST',
+        agent: httpsAgent,
       }
-    )
-
-    const result = await res.json()
-    // console.log(result);
-    toast.notify(`Successfully saved!`, {
-      duration: 5,
-      type: 'success',
-      title: 'Notification'
-    })
+    ).then(async (re: any) => {
+      const result = await re.json()
+      // console.log(result);
+      toast.notify(`Successfully saved!`, {
+        duration: 5,
+        type: 'success',
+        title: 'Notification'
+      })
+    }).catch((e) => {
+      console.log(e);
+    });
   }
 
   return (
